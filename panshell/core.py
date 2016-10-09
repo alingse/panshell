175031e9ceb3d3fa4043d532843f682f3791c2d6
alingse <alingse@foxmail.com>
2016-10-10 00:39:54 +0800

    add:添加 use 并update getattr

--- panshell/core.py
+++ panshell/core.py
@@ -31,7 +31,7 @@ class Shell(cmd.Cmd):
         self.stack = []
         self.fsmap = {}
 
-        self.keywords = ['use']
+        self.keywords = ['use','exit']
 
     def plugin(self,fscls,**kwargs):
         if fscls.__bases__[0] != FS:
@@ -39,24 +39,42 @@ class Shell(cmd.Cmd):
         name = fscls.name
         if name in self.fsmap:
             raise Exception('fs <{}> has already plugin in '.format(name))
-        #try it
+
         tmp = fscls(**kwargs)
         del tmp
         self.fsmap[name] = fscls
 
     def __getattr__(self,attr):
-        if attr not in self.keywords:
-            def f (x,y):
-                print(x,y)
-            if attr.startswith('do_'):
+        if attr.startswith('do_'):
+            attr = attr[3:]
+            if attr not in self.keywords:
+                def f (x,y):
+                    print(x,y)
                 return lambda x:f(self,x)
-                
+
         return cmd.Cmd.__getattr__(self,attr)
 
 
     def do_use(self,name):
-        print(name)
+        """use <fs> 选择使用某个fs
+           
+           use baidu
+           use local
+        """
 
+        if name not in self.fsmap:
+            raise Exception('not plugin in')
+
+        fscls = self.fsmap[name]
+        fs = fscls()
+
+        if self.fs != None:
+            self.stack.append(self.fs)
+
+        self.fs = fs
+
+    def do_exit(self,line):
+        pass
 
     def run(self):
         self.cmdloop()
