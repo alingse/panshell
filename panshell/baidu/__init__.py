57920fb907d20db7b8fb8e23b7ddfd011cbf8fac
alingse <alingse@foxmail.com>
2016-10-10 00:33:07 +0800

    add:log

--- panshell/baidu/__init__.py
+++ panshell/baidu/__init__.py
@@ -6,10 +6,12 @@ from panshell.core import FS
 
 
 class baiduFS(FS):
+    """ baidu yunpan (百度云盘) 文件系统"""
     name = 'baidu'
     def __init__(self,**kwargs):
-        name = kwargs.pop('name',self.name)
+        name = kwargs.pop('name',self.name)        
         FS.__init__(self,name,**kwargs)
+
 
     def do_ls(self,line):
         print(self.name,'ls')
