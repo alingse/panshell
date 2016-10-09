ada31398deae5a25cda102669f0af16c9e351796
alingse <alingse@foxmail.com>
2016-10-09 22:30:59 +0800

    fix:error

--- panshell/local/__init__.py
+++ panshell/local/__init__.py
@@ -8,7 +8,10 @@ class local(FS):
     name = 'local'
     def __init__(self,**kwargs):
         name = kwargs.pop('name',self.name)
-        FS.__init__(_name,**kwargs)
+        FS.__init__(self,name,**kwargs)
 
     def do_ls(self,line):
         print(self.name,'ls')
+
+if __name__ == '__main__':
+    fs = local()
