552ed2077b47e371cc0b3417a67331e65984d9e8
alingse <alingse@foxmail.com>
2016-10-09 22:29:59 +0800

    add:local

---
+++ panshell/local/__init__.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+#author@alingse
+#2016.10.09
+
+from panshell.core import FS
+
+class local(FS):
+    name = 'local'
+    def __init__(self,**kwargs):
+        name = kwargs.pop('name',self.name)
+        FS.__init__(_name,**kwargs)
+
+    def do_ls(self,line):
+        print(self.name,'ls')
